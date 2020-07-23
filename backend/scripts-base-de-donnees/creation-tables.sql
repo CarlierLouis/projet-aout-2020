1f611ad12fcf0ec98a7cebdb7c935c30ee7c099f
Louis Carlier <63374771+CarlouMuhan@users.noreply.github.com>
2020-07-23 14:21:29 +0200

    tables suite

--- backend/scripts-base-de-donnees/creation-tables.sql
+++ backend/scripts-base-de-donnees/creation-tables.sql
@@ -4,3 +4,23 @@ CREATE TABLE "DBA"."administrateurs" (
 	"mp" VARCHAR(30) NULL,
 	PRIMARY KEY ( "id_administrateur" ASC )
 ) IN "system";
+
+
+CREATE TABLE "DBA"."typesMets" (
+	"id_typeMet" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
+	"nomTypeMet" VARCHAR(100) NULL,
+	PRIMARY KEY ( "id_typeMet" ASC )
+) IN "system";
+
+CREATE TABLE "DBA"."mets" (
+	"id_met" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
+	"nomMet" VARCHAR(100) NOT NULL UNIQUE,
+	"description" LONG VARCHAR NULL,
+	"id_typeMet" INTEGER NULL,
+	PRIMARY KEY ( "id_met" ASC )
+) IN "system";
+
+
+
+ALTER TABLE "DBA"."mets" ADD CONSTRAINT "typesMets" FOREIGN KEY ( "id_typeMet" ASC ) REFERENCES "DBA"."typesMets" ( "id_typeMet" );
+
--- frontend/js/site.js
+++ frontend/js/site.js
@@ -3,14 +3,14 @@ function connect() {
     let pseudo = fo.nom.value;
     let mp = fo.mp.value;
     let xhr = new XMLHttpRequest();
-    xhr.open('get','http://localhost/adminConnexion',true);
-    xhr.onload=function() {
-        xhr=JSON.parse(xhr.response);
-        for(let i=0; i<xhr.length; i++) {
-            if((xhr[i].pseudo===pseudo) && (xhr[i].mp===mp)){
-                document.location.href ="modifCarte";
+    xhr.open('get', 'http://localhost/adminConnexion', true);
+    xhr.onload = function () {
+        xhr = JSON.parse(xhr.response);
+        for (let i = 0; i < xhr.length; i++) {
+            if ((xhr[i].pseudo===pseudo) && (xhr[i].mp===mp)) {
+                document.location.href = "modifCarte";
             }
-            else{
+            else {
                 document.getElementById("verif").innerHTML = "Pseudo ou mot de passe incorrect";
             }
         }
@@ -19,4 +19,4 @@ function connect() {
     xhr.send();
     console.log("erreur");
     return false;
-
+}
